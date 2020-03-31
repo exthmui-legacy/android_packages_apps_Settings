273b47e310b13f5a3e89a3a24d18d1a4d8ef1b44
cjybyjk <cjybyjk@gmail.com>
2020-03-31 17:08:13 +0800

    import exTHmSettings
    Signed-off-by: cjybyjk <cjybyjk@gmail.com>

--- Android.mk
+++ Android.mk
@@ -21,6 +21,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../exTHmSettings/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx-constraintlayout_constraintlayout \
@@ -53,7 +54,13 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     zxing-core-1.7 \
     org.lineageos.platform.internal
 
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    packages/apps/exTHmSettings/res
+
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages org.exthmui.settings
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
@@ -71,6 +78,7 @@ include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
     contextualcards:libs/contextualcards.aar
+
 include $(BUILD_MULTI_PREBUILT)
 
 # Use the following include to make our test apk.
