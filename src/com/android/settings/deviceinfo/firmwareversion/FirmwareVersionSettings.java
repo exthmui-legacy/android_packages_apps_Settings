b498960a3f2801e4e0c944dead350ba802ca2894
Col_or <1638876934@qq.com>
2021-01-28 17:25:06 +0800

    FirmwareVersionSettings: use new device_info page

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionSettings.java
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionSettings.java
@@ -18,23 +18,31 @@ package com.android.settings.deviceinfo.firmwareversion;
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.content.Intent;
 import android.provider.SearchIndexableResource;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.deviceinfo.BuildNumberPreferenceController;
+import com.android.settings.deviceinfo.DeviceNamePreferenceController;
+import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable
-public class FirmwareVersionSettings extends DashboardFragment {
+public class FirmwareVersionSettings extends DashboardFragment implements DeviceNamePreferenceController.DeviceNamePreferenceHost {
+
+    private BuildNumberPreferenceController mBuildNumberPreferenceController;
 
     @Override
     protected int getPreferenceScreenResId() {
-        return R.xml.firmware_version;
+        return R.xml.exthm_device_info;
     }
 
     @Override
@@ -55,10 +63,48 @@ public class FirmwareVersionSettings extends DashboardFragment {
                     final ArrayList<SearchIndexableResource> result = new ArrayList<>();
 
                     final SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.firmware_version;
+                    sir.xmlResId = R.xml.exthm_device_info;
                     result.add(sir);
                     return result;
                 }
 
             };
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        use(DeviceNamePreferenceController.class).setHost(this /* parent */);
+        mBuildNumberPreferenceController = use(BuildNumberPreferenceController.class);
+        mBuildNumberPreferenceController.setHost(this /* parent */);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (mBuildNumberPreferenceController.onActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void onSetDeviceNameConfirm(boolean confirm) {
+        final DeviceNamePreferenceController controller = use(DeviceNamePreferenceController.class);
+        controller.updateDeviceName(confirm);
+    }
+
+    @Override
+    public void showDeviceNameWarningDialog(String deviceName) {
+        onSetDeviceNameConfirm(true);
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, this /* fragment */, getSettingsLifecycle());
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, FirmwareVersionSettings fragment, Lifecycle lifecycle) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new SimStatusPreferenceController(context, fragment));
+        return controllers;
+    }
 }
