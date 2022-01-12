93cd4bb5342b576dbc69d730160878b25862bc2c
Col_or <1638876934@qq.com>
2022-01-12 20:59:32 +0800

    Settings: My Device: Hardware Info: Use exthmuix library & Cleanup code
    Change-Id: I6d59cced0f32c37f3e05ef6914214e8331aa3754

--- src/org/exthmui/settings/deviceinfo/hardwareinfo/ExthmHardwareInfoPreferenceController.java
+++ src/org/exthmui/settings/deviceinfo/hardwareinfo/ExthmHardwareInfoPreferenceController.java
@@ -1,29 +1,25 @@
 package org.exthmui.settings.deviceinfo.hardwareinfo;
 
-import android.app.ActivityManager;
 import android.content.Context;
-import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
 import android.os.UserHandle;
-import android.text.TextUtils;
-import android.text.format.Formatter;
-import android.util.Log;
 
 import androidx.preference.PreferenceScreen;
 
-import com.android.settings.core.BasePreferenceController;
 import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
 import com.android.settings.deviceinfo.HardwareInfoPreferenceController;
-import com.android.settings.deviceinfo.StorageCategoryFragment;
-import com.android.settings.deviceinfo.hardwareinfo.SocModelPreferenceController;
 
 import org.exthmui.settings.preference.ExthmHardwareInfoPreference;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+
+import exthmuix.core.util.ExthmDeviceUtils;
+import exthmuix.core.util.ExthmFormatter;
 
 public class ExthmHardwareInfoPreferenceController extends BasePreferenceController {
 
@@ -48,7 +44,7 @@ public class ExthmHardwareInfoPreferenceController extends BasePreferenceControl
                     new ExthmHardwareInfoPreference.HardwareInfo(R.drawable.exthm_settings_ic_hardware_info_phone,
                             mContext.getString(R.string.model_info), HardwareInfoPreferenceController.getDeviceModel()),
                     new ExthmHardwareInfoPreference.HardwareInfo(R.drawable.exthm_settings_ic_hardware_info_storage,
-                            mContext.getString(R.string.exthm_settings_my_device_storage_title), mContext.getString(R.string.exthm_settings_my_device_storage_regex, getMemoryStatus()[0], getMemoryStatus()[1])),
+                            mContext.getString(R.string.exthm_settings_my_device_storage_title), mContext.getString(R.string.exthm_settings_my_device_storage_regex, getStorageInfo()[0], getStorageInfo()[1])),
                     new ExthmHardwareInfoPreference.HardwareInfo(R.drawable.exthm_settings_ic_hardware_info_processor,
                             mContext.getString(R.string.exthm_settings_my_device_processor_title), getProcessorInfo()),
                     new ExthmHardwareInfoPreference.HardwareInfo(R.drawable.exthm_settings_ic_hardware_info_memory,
@@ -60,40 +56,38 @@ public class ExthmHardwareInfoPreferenceController extends BasePreferenceControl
     }
 
     private String getProcessorInfo() {
-        String SOC_MANUFACTURER = null;
-        if (Build.SOC_MANUFACTURER.equals("Qualcomm")) {
-            SOC_MANUFACTURER = mContext.getString(R.string.soc_model_qualcomm);
-        } else if (Build.SOC_MANUFACTURER.equals("Mediatek")) {
-            SOC_MANUFACTURER = mContext.getString(R.string.soc_model_mediatek);
+        String socManufacture = ExthmDeviceUtils.getSocManufacture();
+        String socModel = ExthmDeviceUtils.getSocModel();
+        if (socManufacture == null && socModel == null) {
+            return mContext.getString(R.string.unknown);
         }
-        return SOC_MANUFACTURER + " " + Build.SOC_MODEL;
+        switch (socManufacture.toLowerCase(Locale.ROOT)) {
+            case "qualcomm":
+                socManufacture = mContext.getString(R.string.soc_model_qualcomm);
+                break;
+            case "mediatek":
+                socManufacture = mContext.getString(R.string.soc_model_qualcomm);
+                break;
+            default:
+                if (socModel.startsWith("MT")) {
+                    socManufacture = mContext.getString(R.string.soc_model_qualcomm);
+                } else {
+                    socManufacture = ExthmDeviceUtils.getSocManufacture();
+                }
+        }
+        return socManufacture + " " + socModel;
     }
 
     private String getRamInfo() {
-        ActivityManager actManager = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
-        ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
-        actManager.getMemoryInfo(memInfo);
-        long totRam = memInfo.totalMem;
-        long kb = totRam / 1024;
-        long mb = totRam / 1048576;
-        long gb = totRam / 1073741824;
-        if (gb > 1) {
-            if (totRam % 1073741824 > 0) {
-                return String.valueOf((int) gb + 1).concat(" GB");
-            }
-        } else if (mb > 1) {
-            if (totRam % 1048576 > 0) {
-                return String.valueOf((int) mb + 1).concat(" MB");
-            }
-        } else {
-            if (totRam % 1024 > 0) {
-                return String.valueOf((int) kb + 1).concat(" KB");
-            }
-        }
-        return mContext.getString(R.string.unknown);
+        return ExthmFormatter.formatRamSize(mContext, ExthmDeviceUtils.getTotalRamKiloBytesSize());
     }
 
-    private String[] getMemoryStatus() {
+    /**
+     * To get internal storage info
+     * @return A 2-length string array, the first value is used storage space,,
+     * the second one is total storage size
+     */
+    private String[] getStorageInfo() {
         String externalStorageState = Environment.getExternalStorageState();
         String[] romInfo = new String[2];
         if (externalStorageState.equals("mounted")) {
@@ -101,10 +95,8 @@ public class ExthmHardwareInfoPreferenceController extends BasePreferenceControl
                 StatFs statFs = new StatFs(new Environment.UserEnvironment(UserHandle.myUserId()).getExternalStorageDirectory().getPath());
                 long totalSize = (long) Math.pow(2, ((int) (Math.log((double) statFs.getTotalBytes()) / Math.log(2))) + 1);
                 long usedSize = statFs.getTotalBytes() - statFs.getAvailableBytes();
-                String totalSizeStr = Formatter.formatFileSize(mContext, totalSize, Formatter.FLAG_IEC_UNITS);
-                String usedSizeStr = Formatter.formatFileSize(mContext, usedSize, Formatter.FLAG_IEC_UNITS);
-                romInfo[0] = usedSizeStr;
-                romInfo[1] = totalSizeStr;
+                romInfo[0] = ExthmFormatter.formatFileSize(mContext, usedSize);
+                romInfo[1] = ExthmFormatter.formatFileSize(mContext, totalSize);
             } catch (IllegalArgumentException ignored) {
             }
         } else {
